f7f10df3eb1fb188ccab1ea3602575a753e6f4f8
Paul Frische <paul@paulfrische.xyz>
2022-11-01 11:01:18 +0100

    Add project generation

--- src/pip/_internal/commands/init.py
+++ src/pip/_internal/commands/init.py
@@ -35,7 +35,7 @@ __pycache__/
 .tox
 .cache """
 
-    pyproject_toml = """ [build-system]
+    pyproject_toml = """[build-system]
 # These are the assumed default build requirements from pip:
 # https://pip.pypa.io/en/stable/reference/pip/#pep-517-and-518-support
 requires = ["setuptools>=43.0.0", "wheel"]
@@ -114,15 +114,15 @@ setup(
     version="2.0.0",
     classifiers=[],
     python_requires=">=3.7, <4",
-    extras_require=\{
+    extras_require={{
         "dev": ["check-manifest"],
         "test": ["coverage"],
-    \},
-    project_urls=\{
+    }},
+    project_urls={{
         "Bug Reports": "https://github.com/your/project/issues",
         "Say Thanks!": "http://saythanks.io/to/example",
         "Source": "https://github.com/your/project/",
-    \},
+    }},
 )
 """
 
@@ -137,7 +137,42 @@ setup(
     def run(self, options: Values, args: List[str]) -> int:
         logger.verbose("Using %s", get_pip_version())
         project_path = pathlib.Path(options.target_dir)
+
+        project_name = input("Project name: ")
+
         if not os.path.exists(project_path.absolute()):
             os.mkdir(project_path.absolute())
+
+        # creating project directories
+        os.mkdir((project_path / "src").absolute())
+        os.mkdir((project_path / "tests").absolute())
+        os.mkdir((project_path / f"src/{project_name}").absolute())
+
+        # TODO: find cleaner way
+        # create empty __init__.py files in src/<project_name> and tests directory
+        open((project_path / f"src/{project_name}/__init__.py").absolute(), "w").close()
+        open((project_path / "tests/__init__.py").absolute(), "w").close()
+
+        # create empty LICENSE.txt
+        open((project_path / "LICENSE.txt").absolute(), "w").close()
+
+        with open((project_path / ".gitignore").absolute(), "w") as gitignore_file:
+            gitignore_file.write(self.gitignore)
+
+        with open((project_path / "README.md").absolute(), "w") as readme_file:
+            readme_file.write(self.generate_readme(project_name))
+
+        with open((project_path / "pyproject.toml").absolute(), "w") as pyproject_file:
+            pyproject_file.write(self.pyproject_toml)
+
+        with open((project_path / "setup.cfg").absolute(), "w") as setup_cfg_file:
+            setup_cfg_file.write(self.setup_cfg)
+
+        with open((project_path / "setup.py").absolute(), "w") as setup_py_file:
+            setup_py_file.write(self.generate_setup_py(project_name))
+
+        with open((project_path / "tox.ini").absolute(), "w") as tox_ini_file:
+            tox_ini_file.write(self.tox_ini)
+
         return SUCCESS
 
